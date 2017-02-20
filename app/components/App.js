6c885d2d4960742842fed8a2035b5e02beff20e8
darryl brown <darryl@2b2d.com>
2017-02-19 21:38:55 -0700

    more

--- app/components/App.js
+++ app/components/App.js
@@ -60,7 +60,257 @@ const App = ({ children }) =>
           </div>
 
       </header>
-    { children }
+
+
+  		{ /* <!-- Content
+  		============================================= --> */ }
+  		<section id="content">
+
+  			<div className="content-wrap">
+
+  				<div className="container clearfix">
+
+  					<div className="row clearfix">
+              { children }
+
+  						<div className="col-md-6  bottommargin">
+  							<i className="i-plain color i-large icon-line2-screen-desktop inline-block"
+                  style={{marginBottom: '20px'}}></i>
+  							<div className="heading-block nobottomborder" style={{marginBottom: '15px'}}>
+  								<span className="before-heading">Experience.</span>
+  								<h4>Criminal Law</h4>
+  							</div>
+  							<p>
+                  <ul className="iconlist">
+    								<li><i className="icon-legal"></i> DUI</li>
+    								<li><i className="icon-legal"></i> Felonies</li>
+    								<li><i className="icon-legal"></i> Misdemeanors</li>
+                    <li><i className="icon-legal"></i> Drugs</li>
+                    <li><i className="icon-legal"></i> Domestic Violence</li>
+                    <li><i className="icon-legal"></i> Juvenile</li>
+                    <li><i className="icon-legal"></i> Traffic Violations</li>
+  							  </ul>
+                </p>
+  						</div>
+
+  						<div className="col-md-6  bottommargin">
+  							<i className="i-plain color i-large icon-line2-energy inline-block"
+                  style={{marginBottom: '20px'}}></i>
+  							<div className="heading-block nobottomborder"
+                  style={{marginBottom: '15px'}}>
+  								<span className="before-heading">Local.</span>
+  								<h4>Family Law</h4>
+  							</div>
+  							<p>
+
+                <ul className="iconlist">
+                  <li><i className="icon-legal"></i> Divorce</li>
+                  <li><i className="icon-legal"></i> Parental Responsibility / Custody</li>
+                  <li><i className="icon-legal"></i> Paternity</li>
+                  <li><i className="icon-legal"></i> Grandparent Rights</li>
+                </ul>
+                </p>
+  						</div>
+
+  					</div>
+
+  					<div className="line notopmargin"></div>
+
+  					<h3 className="center">Some <span>Latest</span> Works</h3>
+
+  					<div id="oc-portfolio" className="owl-carousel portfolio-carousel carousel-widget" data-margin="1" data-loop="true" data-nav="true" data-pagi="false"data-items-xxs="1" data-items-xs="2" data-items-sm="3" data-items-md="4" data-items-lg="4">
+
+  						<div className="oc-item">
+  							<div className="iportfolio">
+  								<div className="portfolio-image">
+  									<a href="portfolio-single.html">
+  										<img src="images/portfolio/4/1.jpg" alt="Open Imagination" />
+  									</a>
+  									<div className="portfolio-overlay">
+  										<a href="images/portfolio/full/1.jpg" className="left-icon" data-lightbox="image"><i className="icon-line-plus"></i></a>
+  										<a href="portfolio-single.html" className="right-icon"><i className="icon-line-ellipsis"></i></a>
+  									</div>
+  								</div>
+  								<div className="portfolio-desc">
+  									<h3><a href="portfolio-single.html">Open Imagination</a></h3>
+  									<span><a href="#">Media</a>, <a href="#">Icons</a></span>
+  								</div>
+  							</div>
+  						</div>
+
+  						<div className="oc-item">
+  							<div className="iportfolio">
+  								<div className="portfolio-image">
+  									<a href="portfolio-single.html">
+  										<img src="images/portfolio/4/2.jpg" alt="Locked Steel Gate" />
+  									</a>
+  									<div className="portfolio-overlay">
+  										<a href="images/portfolio/full/2.jpg" className="left-icon" data-lightbox="image"><i className="icon-line-plus"></i></a>
+  										<a href="portfolio-single.html" className="right-icon"><i className="icon-line-ellipsis"></i></a>
+  									</div>
+  								</div>
+  								<div className="portfolio-desc">
+  									<h3><a href="portfolio-single.html">Locked Steel Gate</a></h3>
+  									<span><a href="#">Illustrations</a></span>
+  								</div>
+  							</div>
+  						</div>
+
+  						<div className="oc-item">
+  							<div className="iportfolio">
+  								<div className="portfolio-image">
+  									<a href="#">
+  										<img src="images/portfolio/4/3.jpg" alt="Mac Sunglasses" />
+  									</a>
+  									<div className="portfolio-overlay">
+  										<a href="http://vimeo.com/89396394" className="left-icon" data-lightbox="iframe"><i className="icon-line-play"></i></a>
+  										<a href="portfolio-single-video.html" className="right-icon"><i className="icon-line-ellipsis"></i></a>
+  									</div>
+  								</div>
+  								<div className="portfolio-desc">
+  									<h3><a href="portfolio-single-video.html">Mac Sunglasses</a></h3>
+  									<span><a href="#">Graphics</a>, <a href="#">UI Elements</a></span>
+  								</div>
+  							</div>
+  						</div>
+
+  						<div className="oc-item">
+  							<div className="iportfolio">
+  								<div className="portfolio-image">
+  									<a href="portfolio-single.html">
+  										<img src="images/portfolio/4/5.jpg" alt="Console Activity" />
+  									</a>
+  									<div className="portfolio-overlay">
+  										<a href="images/portfolio/full/5.jpg" className="left-icon" data-lightbox="image"><i className="icon-line-plus"></i></a>
+  										<a href="portfolio-single.html" className="right-icon"><i className="icon-line-ellipsis"></i></a>
+  									</div>
+  								</div>
+  								<div className="portfolio-desc">
+  									<h3><a href="portfolio-single.html">Console Activity</a></h3>
+  									<span><a href="#">UI Elements</a>, <a href="#">Media</a></span>
+  								</div>
+  							</div>
+  						</div>
+
+  						<div className="oc-item">
+  							<div className="iportfolio">
+  								<div className="portfolio-image">
+  									<a href="portfolio-single-video.html">
+  										<img src="images/portfolio/4/7.jpg" alt="Backpack Contents" />
+  									</a>
+  									<div className="portfolio-overlay">
+  										<a href="http://www.youtube.com/watch?v=kuceVNBTJio" className="left-icon" data-lightbox="iframe"><i className="icon-line-play"></i></a>
+  										<a href="portfolio-single-video.html" className="right-icon"><i className="icon-line-ellipsis"></i></a>
+  									</div>
+  								</div>
+  								<div className="portfolio-desc">
+  									<h3><a href="portfolio-single-video.html">Backpack Contents</a></h3>
+  									<span><a href="#">UI Elements</a>, <a href="#">Icons</a></span>
+  								</div>
+  							</div>
+  						</div>
+
+  						<div className="oc-item">
+  							<div className="iportfolio">
+  								<div className="portfolio-image">
+  									<a href="portfolio-single.html">
+  										<img src="images/portfolio/4/8.jpg" alt="Sunset Bulb Glow" />
+  									</a>
+  									<div className="portfolio-overlay">
+  										<a href="images/portfolio/full/8.jpg" className="left-icon" data-lightbox="image"><i className="icon-line-plus"></i></a>
+  										<a href="portfolio-single.html" className="right-icon"><i className="icon-line-ellipsis"></i></a>
+  									</div>
+  								</div>
+  								<div className="portfolio-desc">
+  									<h3><a href="portfolio-single.html">Sunset Bulb Glow</a></h3>
+  									<span><a href="#">Graphics</a></span>
+  								</div>
+  							</div>
+  						</div>
+
+  						<div className="oc-item">
+  							<div className="iportfolio">
+  								<div className="portfolio-image">
+  									<a href="portfolio-single-video.html">
+  										<img src="images/portfolio/4/10.jpg" alt="Study Table" />
+  									</a>
+  									<div className="portfolio-overlay">
+  										<a href="http://vimeo.com/91973305" className="left-icon" data-lightbox="iframe"><i className="icon-line-play"></i></a>
+  										<a href="portfolio-single-video.html" className="right-icon"><i className="icon-line-ellipsis"></i></a>
+  									</div>
+  								</div>
+  								<div className="portfolio-desc">
+  									<h3><a href="portfolio-single-video.html">Study Table</a></h3>
+  									<span><a href="#">Graphics</a>, <a href="#">Media</a></span>
+  								</div>
+  							</div>
+  						</div>
+
+  						<div className="oc-item">
+  							<div className="iportfolio">
+  								<div className="portfolio-image">
+  									<a href="portfolio-single.html">
+  										<img src="images/portfolio/4/11.jpg" alt="Workspace Stuff" />
+  									</a>
+  									<div className="portfolio-overlay">
+  										<a href="images/portfolio/full/11.jpg" className="left-icon" data-lightbox="image"><i className="icon-line-plus"></i></a>
+  										<a href="portfolio-single.html" className="right-icon"><i className="icon-line-ellipsis"></i></a>
+  									</div>
+  								</div>
+  								<div className="portfolio-desc">
+  									<h3><a href="portfolio-single.html">Workspace Stuff</a></h3>
+  									<span><a href="#">Media</a>, <a href="#">Icons</a></span>
+  								</div>
+  							</div>
+  						</div>
+
+  					</div>
+
+
+  					<div className="line"></div>
+
+  					<h3 className="center">What <span>Clients</span> Say</h3>
+
+  					<div className="fslider testimonial testimonial-full noshadow noborder nopadding divcenter"
+              data-animation="fade" data-arrows="false"
+              style={{maxWidth: '700px'}}>
+  						<div className="flexslider">
+  							<div className="slider-wrap">
+  								<div className="slide">
+  									<div className="testi-content">
+  										<p>Similique fugit repellendus expedita excepturi iure perferendis provident quia eaque. Repellendus, vero numquam?</p>
+  										<div className="testi-meta">
+  											Steve Jobs
+  											<span>Apple Inc.</span>
+  										</div>
+  									</div>
+  								</div>
+  								<div className="slide">
+  									<div className="testi-content">
+  										<p>Natus voluptatum enim quod necessitatibus quis expedita harum provident eos obcaecati id culpa corporis molestias.</p>
+  										<div className="testi-meta">
+										Collis Taeed
+										<span>Envato Inc.</span>
+									</div>
+								</div>
+							</div>
+							<div className="slide">
+								<div className="testi-content">
+									<p>Incidunt deleniti blanditiis quas aperiam recusandae consequatur ullam quibusdam cum libero illo rerum!</p>
+									<div className="testi-meta">
+										John Doe
+										<span>XYZ Inc.</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+          </div>
+          </div>
+          </div>
+
+		  </section> { /* <!-- #content end --> */ }
+
     <footer className={footer}>
     </footer>
   </div>;
